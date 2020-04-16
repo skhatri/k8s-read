0d5c889b5ebb5b04a6d7e5a2b1bded510344ab17
Suresh Khatri <servlet2@msn.com>
2020-04-16 22:28:35 +1000

    update failing tests

--- controller/controllers_test.go
+++ controller/controllers_test.go
@@ -11,22 +11,41 @@ type MockApiConfigurer struct {
 	m map[string]router.HandlerFunc
 }
 
-func (mc *MockApiConfigurer) Get(uri string, hf router.HandlerFunc) {
+func (mc *MockApiConfigurer) Get(uri string, hf router.HandlerFunc) router.ApiConfigurer {
 
 	mc.m[makeKey("get", uri)] = hf
+	return mc
 }
 
 func makeKey(method string, uri string) string {
 	return fmt.Sprintf("%s%s", method, uri)
 }
 
-func (mc *MockApiConfigurer) Post(uri string, hf router.HandlerFunc) {
+func (mc *MockApiConfigurer) Post(uri string, hf router.HandlerFunc) router.ApiConfigurer {
 	mc.m[makeKey("post", uri)] = hf
+	return mc
 }
 
-func (mc *MockApiConfigurer) Method(method string, uri string, hf router.HandlerFunc) {
+func (mc *MockApiConfigurer) Method(method string, uri string, hf router.HandlerFunc) router.ApiConfigurer {
 	mc.m[makeKey(method,uri)] = hf
+	return mc
 }
+
+func (mc *MockApiConfigurer) GetIf(cond bool) *router.ConditionalMethodBuilder {
+	return &router.ConditionalMethodBuilder{
+		Method: "GET",
+		Check:    cond,
+		Delegate: nil,
+	}
+}
+func (mc *MockApiConfigurer) PostIf(cond bool) *router.ConditionalMethodBuilder {
+	return &router.ConditionalMethodBuilder{
+		Method: "POST",
+		Check:    cond,
+		Delegate: nil,
+	}
+}
+
 
 func TestRegistersApis(t *testing.T) {
 	m := make(map[string]router.HandlerFunc)
